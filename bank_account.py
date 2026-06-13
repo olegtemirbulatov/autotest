ab1b0175eb0d7376acf90a9a05e1e38bee7195c3
Oleg Temirbulatov <olegtemirbulatov99@gmail.com>
2026-06-13 22:10:38 +0300

    Added tests for bank account sample

---
+++ bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount():
+    def __init__(self, owner: str, balance: float):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, balance: int):
+        if balance < 0:
+            raise ValueError("Сумма должна быть положительной")
+        self.balance += balance
+    
+    def withdraw(self, balance: int):
+        if balance < 0:
+            raise ValueError("Сумма должна быть положительной")
+        if balance > self.balance:
+            raise ValueError("Сумма не может быть больше, чем есть на счете")
+        self.balance -= balance
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,1 +1,35 @@
-assert 2 + 2 == 4
+import pytest
+from bank_account import BankAccount
+
+
+@pytest.fixture
+def account():
+    return BankAccount("Ivan", 1000)
+
+
+@pytest.mark.parametrize("balance, account_balance", [
+    (-1, ValueError),
+    (0, 1000),
+    (1, 999)
+])
+def test_withdraw(account, balance, account_balance):
+    if account_balance is ValueError:
+        with pytest.raises(ValueError):
+            account.withdraw(balance)
+    else:
+        account.withdraw(balance)
+        assert account.balance == account_balance
+
+
+@pytest.mark.parametrize("balance, account_balance", [
+    (-1, ValueError),
+    (0, 1000),
+    (1, 1001)
+])
+def test_deposit(account, balance, account_balance):
+    if account_balance is ValueError:
+        with pytest.raises(ValueError):
+            account.deposit(balance)
+    else:
+        account.deposit(balance)
+        assert account.balance == account_balance
