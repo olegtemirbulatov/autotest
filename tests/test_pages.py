31c7c8ec0afb8d3afede81eb6caf4cf075ea98a8
Oleg Temirbulatov <olegtemirbulatov99@gmail.com>
2026-06-28 15:48:11 +0300

    Added fixture for getting credentials. Excluded load_dotenv() from fixture.

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -5,15 +5,16 @@ from pages.login_page import LoginPage
 
 
 @pytest.mark.ui
-def test_correct_sign_in(login_page: LoginPage):
-    username = os.getenv("GH_USER")
-    password = os.getenv("GH_PASS")
+def test_correct_sign_in(login_page: LoginPage, gh_credentials: tuple[str, str]):
+    username, password = gh_credentials
+    login_page.open()
     login_page.login(username, password)
     login_page.expect_login_succeeded()
 
 @pytest.mark.ui
 def test_incorrect_sign_in(login_page: LoginPage):
     fake = Faker()
+    login_page.open()
     login_page.login(fake.email(), fake.password())
     login_page.expect_login_failed()
     
