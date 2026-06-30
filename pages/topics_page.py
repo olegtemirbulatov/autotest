23c567c3e6550f1a421e4d0f9859f43e8646dc72
Oleg Temirbulatov <olegtemirbulatov99@gmail.com>
2026-06-30 10:10:22 +0300

    Small fix

--- pages/topics_page.py
+++ pages/topics_page.py
@@ -3,5 +3,5 @@ from pages.base_page import BasePage
 
 class TopicsPage(BasePage):
 
-    def get_popular_topics(self):
+    def get_popular_topics(self) -> list[str]:
         return self.page.locator("//a[contains(@href, '/topics/')]/p[1]").all_inner_texts()
